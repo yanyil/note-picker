670d9eb48e367f7b63d1e77e4c9650ca031c79e3
Yan-Yi Li <liyanyi@gmail.com>
2017-02-13 20:58:53 +0000

    Add notes on the frontend
    - select note from a dropdown menu

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,19 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:name)
+  end
 end
--- spec/features/notes_feature_spec.rb
+++ spec/features/notes_feature_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 feature 'notes' do
-  context 'no notes have been selected' do
+  context 'no notes have been added' do
     scenario 'should display a prompt to add notes' do
       visit '/notes'
-      expect(page).to have_content 'No notes have been selected yet'
+      expect(page).to have_content 'No notes have been added yet'
       expect(page).to have_link 'Add notes'
     end
   end
 
-  context 'notes have been selected' do
+  context 'notes have been added' do
     before do
       Note.create(name: 'C')
     end
@@ -17,7 +17,18 @@ feature 'notes' do
     scenario 'display notes' do
       visit '/notes'
       expect(page).to have_content('C')
-      expect(page).not_to have_content('No notes have been selected yet')
+      expect(page).not_to have_content('No notes have been added yet')
+    end
+  end
+
+  context 'adding notes' do
+    scenario 'prompts user to select notes, then displays the new notes' do
+      visit '/notes'
+      click_link 'Add notes'
+      select 'C'
+      click_button 'Add notes'
+      expect(page).to have_content 'C'
+      expect(current_path).to eq '/notes'
     end
   end
 end
